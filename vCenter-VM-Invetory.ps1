54ba5f080e03377b29b1abdc0cb8cf8e3342adc5
Abdulmuqtadir9950 <ab.muqtadir9950@gmail.com>
2026-06-16 15:17:56 +0530

    Update vCenter-VM-Invetory.ps1-5

--- vCenter-VM-Invetory.ps1
+++ vCenter-VM-Invetory.ps1
@@ -1,23 +1,21 @@
 $ErrorActionPreference = "Stop"
 
-Write-Host "=== VM Inventory Start ==="
+Import-Module VMware.VimAutomation.Core
 
-Import-Module VMware.VimAutomation.Core -Force
-
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
+Set-PowerCLIConfiguration -Scope User -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
 Set-PowerCLIConfiguration -Scope User -ParticipateInCEIP $false -Confirm:$false | Out-Null
 
-Write-Host "Connecting to vCenter: $env:VC_SERVER"
+Write-Host "Connecting to vCenter..."
 
 $securePass = ConvertTo-SecureString $env:VC_PASS -AsPlainText -Force
-$cred = New-Object System.Management.Automation.PSCredential ($env:VC_USER, $securePass)
+$cred = New-Object System.Management.Automation.PSCredential($env:VC_USER, $securePass)
 
 Connect-VIServer -Server $env:VC_SERVER -Credential $cred
 
-$vms = Get-VM | Select Name, PowerState, NumCpu, MemoryGB
+Write-Host "Fetching VMs..."
 
-$vms | Export-Csv "$env:WORKSPACE/vcenter_vm_list.csv" -NoTypeInformation
+Get-VM |
+Select Name, PowerState, NumCpu, MemoryGB |
+Export-Csv "$env:WORKSPACE/vcenter_vm_list.csv" -NoTypeInformation
 
 Disconnect-VIServer * -Confirm:$false
-
-Write-Host "DONE"
